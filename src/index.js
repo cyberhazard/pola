09d0108c81f924ab679dbc7d94c198ed3dafbe80
cyber <cyber>
2017-08-14 17:23:04 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import { render } from 'react-dom';
+import { injectGlobal } from 'styled-components';
+
+render(
+  <div>TEST</div>,
+  document.querySelector('#root'),
+);
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    text-decoration: none;
+  }
+`;
