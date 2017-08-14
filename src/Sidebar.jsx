b2a97a6e978c9b397acd3c2fbf7758a371563cce
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-08-15 01:35:57 +0700

    Sidebar some fixes

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Logo from './Logo';
+import Menu from './Menu';
+import Socials from './Socials';
 
 const Sidebar = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 310px;
   flex: 0 0 310px;
   height: 100%;
   border: 2px solid black;
   font-family: 'Roboto';
   font-weight: 400;
+  background: #000d16 url('${require('./_assets/images/decor_bottom.png')}') left bottom no-repeat;
 `;
 
 export default () => (
   <Sidebar>
     <Logo />
-    {/* <Logo />
-    <Menu /> */}
-    Hello World!
+    <Menu />
+    <Socials />
   </Sidebar>
 );
