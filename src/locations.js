473ba73df85a89066e6e59f80fc2028f97d4ea05
cyber <cyber>
2017-08-18 00:17:08 +0300

    locations file

---
+++ src/locations.js
@@ -0,0 +1,26 @@
+export default {
+  moscow: {
+    RU: {
+      title: 'Москва',
+      address: 'Пресненская наб. д.6 стр. 2',
+      phone: '+7 (495) 055-12-12',
+    },
+    EN: {
+      title: 'Moscow',
+      address: 'Presnenskaya nab. 6 b. 2',
+      phone: '+7 (495) 055-12-12',
+    },
+  },
+  ny: {
+    RU: {
+      title: 'Нью-Йорк',
+      address: '9-я East Street',
+      phone: '(212) 348-2626',
+    },
+    EN: {
+      title: 'New-York',
+      address: '9 East Street',
+      phone: '(212) 348-2626',
+    },
+  },
+};
