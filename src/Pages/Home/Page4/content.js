262e0c1affd1afc11344a57dff96e35a3b4b99d4
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-08-18 21:25:18 +0700

    content for contacts

---
+++ src/Pages/Home/Page4/content.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 0,
+    city: 'Москва',
+    adres: 'Пресненская наб. д.6 стр. 2',
+    phone: '+7 (495) 055-12-12',
+    email: 'moscow@pola-group.com',
+  },
+  {
+    id: 1,
+    city: 'Санкт-Петербург',
+    adres: 'Пресненская наб. д.6 стр. 2',
+    phone: '+7 (495) 055-12-12',
+    email: 'moscow@pola-group.com',
+  },
+  {
+    id: 2,
+    city: 'Антверпен',
+    adres: 'Пресненская наб. д.6 стр. 2',
+    phone: '+7 (495) 055-12-12',
+    email: 'moscow@pola-group.com',
+  },
+];
