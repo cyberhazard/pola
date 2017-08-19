0b1f4ec0addf0bf53119def75dcee20ced8b695a
cyber <cyber>
2017-08-19 22:11:20 +0300

    breadcrumbs

---
+++ src/Components/Breadcrumbs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { Icon } from 'react-fa';
+
+const Breadcrumbs = styled.div`
+
+`;
+
+const Root = styled(Link)`
+  color: rgb(0, 0, 0);
+  font-family: Roboto;
+  font-size: 1.5rem;
+  font-weight: 400;
+`;
+
+const Angle = styled(Icon)`
+  font-size: 1.5rem;
+  margin: 0 1.1rem;
+`;
+
+const Current = styled.span`
+  color: rgb(158, 158, 158);
+  font-family: Roboto;
+  font-size: 1.5rem;
+  font-weight: 400;
+`;
+
+export default ({ to, label, current }) => (
+  <Breadcrumbs>
+    <Root to={to}>{label}</Root><Angle name="angle-right" /><Current>{current}</Current>
+  </Breadcrumbs>
+);
