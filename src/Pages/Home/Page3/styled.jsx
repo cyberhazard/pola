79560799c5c804bb6efec1925bd7e4b7a9a19418
cyber <cyber>
2017-08-19 15:03:55 +0300

    add backgrounds for page3

--- src/Pages/Home/Page3/styled.jsx
+++ src/Pages/Home/Page3/styled.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import { Page as OriginalPage } from './../styled';
 
+import bg1 from './../../../_assets/images/home-page3-bg-rect.png';
+import bg2 from './../../../_assets/images/home-page3-bg-rect2.png';
+
 export const Page = styled(OriginalPage)`
   display: flex;
   flex-direction: column;
@@ -10,6 +13,9 @@ export const Page = styled(OriginalPage)`
   padding-right: 18.6335403727%;
   padding-left: 10rem;
   background-color: #004980;
+  background-image: url('${bg1}') , url('${bg2}');
+  background-position: bottom right, top left;
+  background-repeat: no-repeat, no-repeat;
 `;
 
 export const Header = styled.h2`
