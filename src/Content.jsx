bdfa3c45dd2b237a88570beb893a1bf2fe8c7c75
cyber <cyber>
2017-08-19 20:44:29 +0300

    add: not found page

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import About from './Pages/About';
@@ -9,6 +9,7 @@ import Contacts from './Pages/Contacts';
 import News from './Pages/News';
 import Services from './Pages/Services';
 import Stockholders from './Pages/Stockholders';
+import NotFound from './Pages/NotFound';
 
 const Content = styled.div`
   flex-grow: 1;
@@ -17,12 +18,15 @@ const Content = styled.div`
 
 export default () => (
   <Content>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/actives" component={Actives} />
-    <Route path="/contacts" component={Contacts} />
-    <Route path="/news" component={News} />
-    <Route path="/services" component={Services} />
-    <Route path="/stockholders" component={Stockholders} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route path="/actives" component={Actives} />
+      <Route path="/contacts" component={Contacts} />
+      <Route path="/news" component={News} />
+      <Route path="/services" component={Services} />
+      <Route path="/stockholders" component={Stockholders} />
+      <Route path="*" component={NotFound} />
+    </Switch>
   </Content>
 );
