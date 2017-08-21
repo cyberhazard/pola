bd0b2cf19721406ca5a2de20664aa70f2a0620ed
cyber <cyber>
2017-08-22 02:08:02 +0300

    add navigation buttons

--- src/Pages/About/Lead/Slider/index.jsx
+++ src/Pages/About/Lead/Slider/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon as Ficon } from 'react-fa';
 import styled from 'styled-components';
 import Person from './Person';
 import persons from './data';
@@ -6,10 +7,7 @@ import persons from './data';
 const Slider = styled.div`
   width: 100%;
   position: relative;
-`;
-
-const Paginations = styled.div`
-
+  margin-bottom: 6rem;
 `;
 
 const Content = styled.div`
@@ -24,24 +22,48 @@ const MovingBlock = styled.div`
   ${p => p.offset && `transform: translateX(-${p.offset}rem);`}
 `;
 
+const Icon = styled(Ficon)`
+  font-size: 3.3rem;
+`;
+
+const Button = styled.div`
+  position: absolute;
+  top: 22rem;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  width: 4.3rem;
+  height: 4.3rem;
+  box-shadow: 0 0.8rem 1.8rem rgba(0, 0, 0, 0.12);
+  background-color: rgb(7, 107, 181);
+  transition: .3s;
+  cursor: pointer;
+  ${p => p.left && 'transform: rotate(180deg); left: 0; right: auto;'}
+  &:hover {
+    background-color: hsl(206, 93%, 47%);
+  }
+`;
+
 export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = { currentOffset: 0 };
     this.blockOffset = 27.6;
-    this.maxOffset = this.blockOffset * (persons.length - 1);
+    this.maxOffset = this.blockOffset * (persons.length - 5);
     this.next = this.next.bind(this);
     this.prev = this.prev.bind(this);
   }
 
   next() {
     if (this.state.currentOffset >= this.maxOffset) return null;
-    this.setState(p => ({ currentOffset: p.currentOffset + this.blockOffset }));
+    return this.setState(p => ({ currentOffset: p.currentOffset + this.blockOffset }));
   }
 
   prev() {
     if (this.state.currentOffset === 0) return null;
-    this.setState(p => ({ currentOffset: p.currentOffset - this.blockOffset }));
+    return this.setState(p => ({ currentOffset: p.currentOffset - this.blockOffset }));
   }
 
   render() {
@@ -61,7 +83,8 @@ export default class extends React.Component {
             }
           </MovingBlock>
         </Content>
-        <Paginations />
+        <Button onClick={this.prev} left><Icon name="angle-right" /></Button>
+        <Button onClick={this.next} ><Icon name="angle-right" /></Button>
       </Slider>
     );
   }
