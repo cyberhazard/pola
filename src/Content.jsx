7e671d8556d3a5e214935898794770febbeeea08
cyber <cyber>
2017-08-22 00:57:30 +0300

    add Lead page

--- src/Content.jsx
+++ src/Content.jsx
@@ -12,6 +12,7 @@ import Stockholders from './Pages/Stockholders';
 import NotFound from './Pages/NotFound';
 import Structure from './Pages/About/Structure';
 import Profile from './Pages/About/Profile';
+import Lead from './Pages/About/Lead';
 
 const Content = styled.div`
   flex-grow: 1;
@@ -26,6 +27,7 @@ export default () => (
       <Route exact path="/about" component={About} />
       <Route path="/about/structure" component={Structure} />
       <Route path="/about/profile" component={Profile} />
+      <Route path="/about/lead" component={Lead} />
       <Route path="/actives" component={Actives} />
       <Route path="/contacts" component={Contacts} />
       <Route path="/news" component={News} />
