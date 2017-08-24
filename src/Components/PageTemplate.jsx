1f668980540261bcff9db20fa2071cb0bd0bf841
cyber <cyber>
2017-08-25 00:58:54 +0300

    add some propeties to PageTemplate

--- src/Components/PageTemplate.jsx
+++ src/Components/PageTemplate.jsx
@@ -6,14 +6,16 @@ export default styled.div`
   justify-content: space-between;
   position: relative;
   height: 93.3333333333vh;
-  ${({ full }) => full && `
-    height: auto;
-    min-height: 93.3333333333vh;
-  `}
   width: 100%;
   padding-top: 6.2626262626vh;
   padding-left: 10rem;
   padding-right: 23rem;
   padding-bottom: 10rem;
   background-color: white;
+  ${({ full }) => full && `
+    height: auto;
+    min-height: 93.3333333333vh;
+    display: block;
+    padding-bottom: 7rem;
+  `}
 `;
