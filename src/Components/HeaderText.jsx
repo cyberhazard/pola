1f4506ec354b7aa6ee4237c8a2bc8720a4b86ba4
cyber <cyber>
2017-08-25 00:04:57 +0300

    refactor: HeaderText Component

--- src/Components/HeaderText.jsx
+++ src/Components/HeaderText.jsx
@@ -6,10 +6,14 @@ export default styled.h2`
   font-size: 3.6rem;
   font-weight: 700;
   transform: translateX(-${p => (p.offset ? p.offset : '10rem')});
+  padding-left: 10.6rem;
+  position: relative;
   &:before {
     content: '';
     display: inline-block;
-    margin-right: 2.7rem;
+    position: absolute;
+    top: 2.5rem;
+    left: 0;
     height: 1.1rem;
     width: 7.9rem;
     transform: translateY(-50%);
