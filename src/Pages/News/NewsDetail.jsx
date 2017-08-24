b128cc1aab198f393cdf116bfd423c90e2ac89de
cyber <cyber>
2017-08-25 00:02:43 +0300

    News detail page in progress

--- src/Pages/News/NewsDetail.jsx
+++ src/Pages/News/NewsDetail.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import news from './../../_fake_api/news.js';
+import news from './../../_fake_api/news';
+import PageTemplate from './../../Components/PageTemplate';
+import Header from './../../Components/HeaderText';
 
 export default ({ match }) => {
   const id = match.params.id;
   const data = news.find(n => n.id === id);
   return (
-    <div>Title: {data.title}, ID: {data.id}</div>
-  )
+    <PageTemplate> ID: {data.id}
+      <Header>{data.title}</Header>
+      ID: {data.id}
+    </PageTemplate>
+  );
 };
