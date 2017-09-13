6665098055def34d38450c69b4a3f23642088234
cyber <cyber>
2017-09-14 01:35:59 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './_assets/css/global.css';
+import reducers from './_reducers';
 
 import './_assets/css/fonts.css';
 import App from './App';
 // eslint-disable-next-line
 //import TestComponents from './TestComponents.jsx';
 
+const store = createStore(reducers);
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.querySelector('#root'),
 );
