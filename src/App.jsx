c91ddeffb0b58df120eef55600216294bdf8d9ce
cyber <cyber>
2017-09-14 02:17:36 +0300

    fix: update component state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import { withRouter } from 'react-router-dom';
 
 import Sidebar from './Sidebar';
-import RightSide from './RightSide';
+import RightSideBlocked from './RightSide';
 
 const App = styled.div`
   display: flex;
   width: 100%;
   height: 100vh;
 `;
+
+const RightSide = withRouter(RightSideBlocked);
 
 export default () => (
   <App>
