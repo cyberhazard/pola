f53e789128b5b9c37fe845a5d6e8ca2902a89d11
cyber <cyber>
2017-09-15 00:25:32 +0300

    refactor to map

--- src/Pages/About/Projects/index.jsx
+++ src/Pages/About/Projects/index.jsx
@@ -3,12 +3,7 @@ import styled from 'styled-components';
 import Breadcrumbs from './../../../Components/Breadcrumbs';
 import HeaderText from './../../../Components/HeaderText';
 import Block from './../Structure/Block';
-
-import ship1 from './../../../_assets/images/structure/ship1.png';
-import ship2 from './../../../_assets/images/structure/ship2.png';
-import ship3 from './../../../_assets/images/structure/ship3.png';
-import ship4 from './../../../_assets/images/structure/ship4.png';
-import ship5 from './../../../_assets/images/structure/ship5.png';
+import projects from './../../../_fake_api/projects';
 
 const Page = styled.div`
   position: relative;
@@ -28,30 +23,19 @@ export default () => (
   <Page>
     <Breadcrumbs to="/about" label="О группе" current="Проекты" />
     <Header>Структура группы</Header>
-    <Block header="Проект «RSD-59»" top more="rsd-59" img={ship1}>
-      Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации,
-      например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.
-      Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки...
-    </Block>
-    <Block header="Проект «RSD-59»" top more="rsd-59" img={ship2} right>
-      Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации,
-      например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.
-      Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки...
-    </Block>
-    <Block header="Проект «RSD-59»" top more="rsd-59" img={ship3}>
-      Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации,
-      например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.
-      Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки...
-    </Block>
-    <Block header="Проект «RSD-59»" top more="rsd-59" img={ship4} right>
-      Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации,
-      например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.
-      Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки...
-    </Block>
-    <Block header="Проект «RSD-59»" top more="rsd-59" img={ship5}>
-      Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации,
-      например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь.
-      Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки...
-    </Block>
+    {
+      projects.map((project, index) => (
+        <Block
+          top
+          key={project.name}
+          header={project.name}
+          more={project.name}
+          img={project.photo}
+          right={index % 2 !== 0}
+        >
+          {project.description}
+        </Block>
+      ))
+    }
   </Page>
 );
