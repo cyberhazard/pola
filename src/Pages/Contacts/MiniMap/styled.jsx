0ae1a95ab2d3e303525a059fb1ac1508d1f0aed9
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-09-15 19:22:18 +0700

    minimap block

---
+++ src/Pages/Contacts/MiniMap/styled.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items:center;
+`;
+
+export const MiniMapa = styled.img`
+  margin-right: 1.8rem;
+`;
+
+export const Show = styled.div`
+  color: #ffffff;
+  font-family: Roboto;
+  font-size: 1.8rem;
+  font-weight: 400;
+  border-bottom: 0.3rem dotted #ffffff;
+  cursor: pointer;
+`;
