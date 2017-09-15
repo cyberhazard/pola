aa4dceda332f29eb825a7ff7d65d808534e57421
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-09-15 20:22:08 +0700

    backgrounds contacts

--- src/Pages/Contacts/index.jsx
+++ src/Pages/Contacts/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import CurrentData from './CurrentData';
 import MiniMap from './MiniMap';
 import Feedback from './Feedback';
 import Footer from './Footer';
+import Map from './../Home/Page4';
+import Targets from './../Home/Page4/Targets';
 import { Page, Header, Top, Bottom, LeftSide, RightSide } from './styled';
+import { Contact, Places } from './../Home/Page4/styled';
+
+export const MapBlock = styled(Map)`
+  width:100%;
+`;
 
 export default () => (
   <Page>
@@ -18,6 +26,11 @@ export default () => (
       </RightSide>
     </Top>
     <Bottom>
+      <Contact>
+        <Places>
+          <Targets />
+        </Places>
+      </Contact>
       <Footer />
     </Bottom>
   </Page>
