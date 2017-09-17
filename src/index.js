805905bd87d42ff4c253c521fd4253252cdbd4c1
cyber <cyber>
2017-09-17 13:51:19 +0300

    polyfill

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import 'babel-polyfill';
 import './_assets/css/global.css';
 import reducers from './_reducers';
 
