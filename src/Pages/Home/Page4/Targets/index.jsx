b869a9fc2e8cd02f7a561ffa6a44b0d3dd0a0193
cyber <cyber>
2017-09-18 15:55:59 +0300

    refactor contacts data

--- src/Pages/Home/Page4/Targets/index.jsx
+++ src/Pages/Home/Page4/Targets/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 import { Icon } from 'react-fa';
-import content from './../content';
+// import content from './../content';
 import { Content, Target, Item, Email, City } from './styled';
+import locations from './../../../../locations';
 
 export const Angle = styled(Icon)`
   margin: 0 1.1rem;
   font-size:1.4rem;
 `;
 
-export default class extends React.Component {
+class TargetClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = { selectedId: 0 };
@@ -19,16 +21,17 @@ export default class extends React.Component {
     this.setState({ selectedId });
   }
   render() {
+    const content = Object.keys(locations).map(loc => locations[loc][this.props.language]);
     return (
       <Target>
         {
           content.map(el => (
             <Content
-              selected={el.id === this.state.selectedId}
-              onClick={() => this.handleClick(el.id)}
-              key={el.id}
+              selected={el.title === this.state.selectedId}
+              onClick={() => this.handleClick(el.title)}
+              key={el.title}
             >
-              <City>  {el.city} </City>
+              <City>  {el.title} </City>
               <Item> <Angle name="map-marker" /> {el.address} </Item>
               <Item> <Angle name="phone" /> {el.phone} </Item>
               <Email> <Angle name="envelope" /> {el.email} </Email>
@@ -39,3 +42,9 @@ export default class extends React.Component {
     );
   }
 }
+
+export default connect(
+  state => ({
+    language: state.language,
+  }),
+)(TargetClass);
