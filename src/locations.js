89bf42fecbce6937b92b083a671834ff33d8dbb6
cyber <cyber>
2017-09-18 15:14:19 +0300

    refactor

--- src/locations.js
+++ src/locations.js
@@ -4,11 +4,13 @@ export default {
       title: 'Москва',
       address: 'Пресненская наб. д.6 стр. 2 оф.2525',
       phone: '+7 (495) 055-12-12',
+      email: 'moscow@pola-group.com',
     },
     EN: {
       title: 'Moscow',
       address: 'Presnenskaya nab. 6 b. 2',
       phone: '+7 (495) 055-12-12',
+      email: 'moscow@pola-group.com',
     },
   },
   bg: {
@@ -16,11 +18,13 @@ export default {
       title: 'Бельгия',
       address: 'London Tower, Noorderplaats 9 ',
       phone: '(212) 348-2626',
+      email: 'moscow@pola-group.com',
     },
     EN: {
       title: 'Belgium',
       address: 'London Tower, Noorderplaats 9 ',
       phone: '(212) 348-2626',
+      email: 'moscow@pola-group.com',
     },
   },
   br: {
@@ -28,11 +32,13 @@ export default {
       title: 'Рио-де-Жанейро',
       address: 'Rua General Venancio Flores 305, sala 203 ',
       phone: '(212) 348-2626',
+      email: 'moscow@pola-group.com',
     },
     EN: {
       title: 'Rio de Janeiro',
       address: 'Rua General Venancio Flores 305, sala 203 ',
       phone: '(212) 348-2626',
+      email: 'moscow@pola-group.com',
     },
   },
 };
