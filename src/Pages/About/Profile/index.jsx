4b16cbfb0485ae1024e039331abe86650ccb315e
cyber <cyber>
2017-10-29 16:55:09 +0300

    translate

--- src/Pages/About/Profile/index.jsx
+++ src/Pages/About/Profile/index.jsx
@@ -1,30 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Content, Header, Page, Text } from './styled';
 import Breadscrubs from './../../../Components/Breadcrumbs';
 import Button from './../../../Components/Button';
 import Slider from './Slider';
 
+const t = {
+  RU: {
+    description: <span>Группа компаний Pola специализируется на перевозках наливных, навалочных и генеральных грузов на судах морского и речного флота, а также на судах класса “река-море”. Суммарная численность судов, находящихся в управлении компаний группы Polа, составляет более 90 ед. общим тоннажем более 900 тыс.т. Помимо этого группа обладает собственными судостроительными и судоремонтными мощностями.<br /><br /></span>,
+    list: (<span>Услуги группы компаний Pola<br />
+      - Морские перевозки грузов<br />
+      - Речные перевозки грузов<br />
+      - Пассажирские перевозки и организация речных круизов<br />
+      - Судостроительная и судоремонтная деятельность<br />
+      - Услуги технического и кадрового менеджмента<br />
+      - Комплексные логистические решения<br /></span>),
+  },
+  EN: {
+    description: <span>Pola Group of Companies is specialized in the transportation of bulk and general cargoes on the vessels of the sea and river fleet, as well as on the vessels of the river-sea class. The total number of vessels operated by Pola Group of Companies is more than 90 units with total tonnage over 900 thousand tons. In addition, the group has its own shipbuilding and ship repair facilities.<br /><br /></span>,
+    list: (<span>Services of Pola Group of Companies<br />
+      - Sea freight transportation<br />
+      - River freight transportation<br />
+      - Passenger transportation and river cruise organization<br />
+      - Shipbuilding and ship repair activities<br />
+      - Services of technical and personnel management<br />
+      - Comprehensive logistics solutions<br /></span>),
+  },
+};
 
-export default () => (
+const Profile = ({ l }) => (
   <Page>
-    <Breadscrubs to="/about" label="О Группе" current="Профиль группы" />
-    <Header>Профиль группы</Header>
+    <Breadscrubs to="/about" label={l === 'RU' ? 'О группе' : 'About group'} current={l === 'RU' ? 'Профиль группы' : 'Group\'s profile page'} />
+    <Header>{l === 'RU' ? 'Профиль группы' : 'Group\'s profile page'}</Header>
     <Content>
       <Text>
-        Группа компаний Pola специализируется на перевозках наливных, навалочных и генеральных грузов на судах морского и речного флота, а также на судах класса “река-море”. Суммарная численность судов, находящихся в управлении компаний группы Polа, составляет более 90 ед. общим тоннажем более 900 тыс.т. Помимо этого группа обладает собственными судостроительными и судоремонтными мощностями.<br /><br />
-        Услуги группы компаний Pola<br />
-        - Морские перевозки грузов<br />
-        - Речные перевозки грузов<br />
-        - Пассажирские перевозки и организация речных круизов<br />
-        - Судостроительная и судоремонтная деятельность<br />
-        - Услуги технического и кадрового менеджмента<br />
-        - Комплексные логистические решения<br />
+        {t[l].description}
+        {t[l].list}
       </Text>
       <Slider />
     </Content>
     <Link to="/about/history">
-      <Button label="История группы" />
+      <Button label={l === 'RU' ? 'История группы' : 'History'} />
     </Link>
   </Page>
 );
+
+export default connect(
+  state => ({ l: state.language }),
+)(Profile);
