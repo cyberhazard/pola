d5eef3d9d5bc670cc27e302fc58e852942d6be03
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2017-11-27 17:11:37 +0700

    fixs

--- src/Pages/Contacts/Footer/index.jsx
+++ src/Pages/Contacts/Footer/index.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { Footer, LeftSide, RightSide, Text, Logo } from './styled';
-
-import Spacecode from './../../../_assets/images/spacecode.png';
+import { Footer, LeftSide } from './styled';
 
 export default ({ l }) => (
   <Footer>
     <LeftSide> © 2017 Pola Group. {l === 'RU' ? 'Все права защищены' : 'All rights reserved'}. </LeftSide>
-    <RightSide>
-      <Text>{l === 'RU' ? 'Сайт разработан' : 'Designed by'}</Text>
-      <a href="http://spacecode.ru/">
-        <Logo src={Spacecode} />
-      </a>
-    </RightSide>
   </Footer>
 );
